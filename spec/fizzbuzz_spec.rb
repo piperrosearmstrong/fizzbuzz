e683d950e528c29442dffc0fd3ba2c0e6828a3bf
Sarita <saritaradia65@gmail.com>
2022-09-07 16:19:17 +0100

    Further exercises

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,8 +2,14 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'Fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz' 
   end
+  it 'returns "Buzz" when passed 5' do 
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+  it 'returns "FizzBuzz" when passsed 15' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz' 
+  end 
 end
 
 #Testing push 
