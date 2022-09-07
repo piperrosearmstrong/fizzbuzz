612ff3da94f032659b225fb62ebc1cd52d738142
Sarita <saritaradia65@gmail.com>
2022-09-07 15:28:28 +0100

    Passing the first test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,10 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-<<<<<<< HEAD
 end
 
 #Testing push 
-=======
-end
->>>>>>> 60df2fc7c68f5f38db9332c2429023bb3d08b285
+
